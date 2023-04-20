fd02ac02b9c6bab1116c5ce86df6bffe470a6c8b
Stefan Draeger <stefandraeger@gmail.com>
2023-04-20 14:54:43 +0200

    remove unused parameter

--- AT24C256_EEPROM_lib/at24c256eeprom.h
+++ AT24C256_EEPROM_lib/at24c256eeprom.h
@@ -2,17 +2,15 @@
 #define AT24C_h
 #include "Arduino.h"
 
-#define EEPROM_I2C_ADDRESS 0x50
-
 class AT24C {
   public:
 
   AT24C();
 
-  void write_byte(int deviceaddress, unsigned int eeaddress, byte data);
-  void write_page(int deviceaddress, unsigned int eeaddresspage, byte *data, byte length);
-  byte read_byte(int deviceaddress, unsigned int eeaddress);
-  void read_buffer(int deviceaddress, unsigned int eeaddress, byte *buffer, int length);
+  void write_byte(unsigned int eeaddress, byte data);
+  void write_page(unsigned int eeaddresspage, byte *data, byte length);
+  byte read_byte(unsigned int eeaddress);
+  void read_buffer(unsigned int eeaddress, byte *buffer, int length);
   
   private:
   
