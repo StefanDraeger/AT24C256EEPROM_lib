6eeaaeb596b8d9cc5bdf178f635de164908f0e46
Stefan Draeger <stefandraeger@gmail.com>
2023-04-20 14:41:34 +0200

    implement constant for deviceaddress

--- AT24C256_EEPROM_lib/at24c256eeprom.cpp
+++ AT24C256_EEPROM_lib/at24c256eeprom.cpp
@@ -1,19 +1,21 @@
 #include "at24c256eeprom.h"
 #include <Wire.h>
 
+#define EEPROM_I2C_ADDRESS 0x19
+
 AT24C::AT24C(){ }
 
-void AT24C::write_byte(int deviceaddress, unsigned int eeaddress, byte data){
+void AT24C::write_byte(unsigned int eeaddress, byte data){
 	int rdata = data;
-	Wire.beginTransmission(deviceaddress);
+	Wire.beginTransmission(EEPROM_I2C_ADDRESS);
 	Wire.write((int)(eeaddress >> 8));    // MSB
 	Wire.write((int)(eeaddress & 0xFF));  // LSB
 	Wire.write(rdata);
 	Wire.endTransmission();
 }
 
-void AT24C::write_page(int deviceaddress, unsigned int eeaddresspage, byte *data, byte length){
-	Wire.beginTransmission(deviceaddress);
+void AT24C::write_page(unsigned int eeaddresspage, byte *data, byte length){
+	Wire.beginTransmission(EEPROM_I2C_ADDRESS);
 	Wire.write((int)(eeaddresspage >> 8));    // MSB
 	Wire.write((int)(eeaddresspage & 0xFF));  // LSB
 	byte c;
@@ -22,25 +24,25 @@ void AT24C::write_page(int deviceaddress, unsigned int eeaddresspage, byte *data
 	}
 	Wire.endTransmission();
 }
-byte AT24C::read_byte(int deviceaddress, unsigned int eeaddress){
+byte AT24C::read_byte(unsigned int eeaddress){
 	byte rdata = 0xFF;
-	Wire.beginTransmission(deviceaddress);
+	Wire.beginTransmission(EEPROM_I2C_ADDRESS);
 	Wire.write((int)(eeaddress >> 8));    // MSB
 	Wire.write((int)(eeaddress & 0xFF));  // LSB
 	Wire.endTransmission();
-	Wire.requestFrom(deviceaddress, 1);
+	Wire.requestFrom(EEPROM_I2C_ADDRESS, 1);
 	if (Wire.available()){
 		rdata = Wire.read();
 	} 
 	return rdata;
 }
 
-void AT24C::read_buffer(int deviceaddress, unsigned int eeaddress, byte *buffer, int length){
-	Wire.beginTransmission(deviceaddress);
+void AT24C::read_buffer(unsigned int eeaddress, byte *buffer, int length){
+	Wire.beginTransmission(EEPROM_I2C_ADDRESS);
 	Wire.write((int)(eeaddress >> 8));    // MSB
 	Wire.write((int)(eeaddress & 0xFF));  // LSB
 	Wire.endTransmission();
-	Wire.requestFrom(deviceaddress, length);
+	Wire.requestFrom(EEPROM_I2C_ADDRESS, length);
 	if (Wire.available()){
 		for (int c = 0; c < length; c++){
 			buffer[c] = Wire.read();
